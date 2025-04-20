a56695f10fe9932dce86eedb8aa1a85f1fc12b20
TargetX25 <espinoza1250125@gmail.com>
2025-04-20 20:08:08 +0530

    Minor Fix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -19,21 +19,8 @@ const Header = () => {
     <header className="bg-white font-livvic border-gray-200 p-3">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="lucide lucide-plane-takeoff-icon lucide-plane-takeoff"
-          >
-            <path d="M2 22h20" />
-            <path d="M6.36 17.4 4 17l-2-4 1.1-.55a2 2 0 0 1 1.8 0l.17.1a2 2 0 0 0 1.8 0L8 12 5 6l.9-.45a2 2 0 0 1 2.09.2l4.02 3a2 2 0 0 0 2.1.2l4.19-2.06a2.41 2.41 0 0 1 1.73-.17L21 7a1.4 1.4 0 0 1 .87 1.99l-.38.76c-.23.46-.6.84-1.07 1.08L7.58 17.2a2 2 0 0 1-1.22.18Z" />
-          </svg>
+        <img src="../public/travel-101.svg" className="size-10" alt="My Logo" />
+
           <div
             className="text-2xl font-bold ml-2 pb-2 text-gray-800 cursor-pointer"
             onClick={() => navigate("/")}
